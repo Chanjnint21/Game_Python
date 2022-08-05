2de3ca0d25c6604a299d581bd3a6d6d9f0a91edb
Chanjnint21 <limchanjnint@gmail.com>
2022-08-05 16:03:57 +0700

    updated

--- Guessing02.py
+++ Guessing02.py
@@ -1,7 +1,7 @@
 
 import random
 
-def guessing02():
+def method1():
 
     pick = random.randint(1,10) # randint() method returns an integer number selected element from the specified range
     attempt = 5
@@ -25,41 +25,42 @@ def guessing02():
             print("You lose !!!")
             ask = input("\n Do you want to play again ?(y/n) :")
     if ask == "y" :
-        guessing02()
+        method1()
     if ask == "n" :
         print("Thank you ... !")
+method1()
 
-    # inp = int(input("Enter the number: "))
-    # Item_list = []
-    # for i in range (1, inp+1):
-    #     Item_list.append(int(i))
-    # attempt = 5
-    # print("you have",attempt ,"attempts")
-    # print (Item_list)
-    # while attempt > 0:
-    #     pick = random.choice(Item_list)
-    #     player = int(input("\nOne element you choose: "))
+def method2():
+    inp = int(input("Enter the number: "))
+    Item_list = []
+    for i in range (1, inp+1):
+        Item_list.append(int(i))
+    attempt = 5
+    print("you have",attempt ,"attempts")
+    print (Item_list)
+    while attempt > 0:
+        pick = random.choice(Item_list)
+        player = int(input("\nOne element you choose: "))
         
-    #     if player < pick:
-    #         attempt = attempt - 1
-    #         print("the value is Greater")
-    #         print(attempt,"attempt(s) left")
+        if player < pick:
+            attempt = attempt - 1
+            print("the value is Greater")
+            print(attempt,"attempt(s) left")
 
-    #     elif player > pick:
-    #         attempt = attempt - 1
-    #         print("the value is less")
-    #         print(attempt,"attempt(s) left")
+        elif player > pick:
+            attempt = attempt - 1
+            print("the value is less")
+            print(attempt,"attempt(s) left")
 
-    #     else:
-    #         print("you win !!!!")
-    #         ask = input("Do you want to play again ?")
+        else:
+            print("you win !!!!")
+            ask = input("Do you want to play again ?")
 
-    #     if attempt == 0:
-    #         print("You lose !!!")
-    #         ask = input("Do you want to play again ?")
-    # if ask == "y" :
-    #     guessing02()
-    # if ask == "n" :
-    #     print("Thank you ... !")
-
-guessing02()
+        if attempt == 0:
+            print("You lose !!!")
+            ask = input("Do you want to play again ?")
+    if ask == "y" :
+        method2()
+    if ask == "n" :
+        print("Thank you ... !")
+#method2()
